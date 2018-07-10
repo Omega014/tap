e33dc3c6f8c8a97223a4d6cb18c2e1a8416638f8
Yuta Sakata <yusaka39@gmail.com>
2018-07-10 21:28:47 +0900

    Add counter activity

--- app/src/main/java/com/example/mitsukisugiya/tap/MainActivity.kt
+++ app/src/main/java/com/example/mitsukisugiya/tap/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.mitsukisugiya.tap
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = this.findViewById<TextView>(R.id.count_text)
+        var count: Int by Delegates.observable(0) { _, _, new ->
+            text.text = new.toString()
+        }
+
+        this.findViewById<Button>(R.id.count_up_button).setOnClickListener {
+            count = count + 1
+        }
+        this.findViewById<Button>(R.id.reset_button).setOnClickListener {
+            count = 0
+        }
     }
 }
