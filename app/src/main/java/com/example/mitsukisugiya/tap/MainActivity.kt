67c59a8e039087acb29dc6236040da1054e115cb
Omega014 <metallic.code@gmail.com>
2018-07-13 14:23:56 +0900

    add date and thread

--- app/src/main/java/com/example/mitsukisugiya/tap/MainActivity.kt
+++ app/src/main/java/com/example/mitsukisugiya/tap/MainActivity.kt
@@ -5,6 +5,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.util.Log
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,18 +20,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        // date
+        val date: Date = Date()
+        val df = SimpleDateFormat("yyyy/MM/dd")
+        val date_view: TextView = this.findViewById(R.id.date)
+        date_view.text = df.format(date)
+
+
         val count_button: Button = this.findViewById(R.id.count)
         val reset_button: Button = this.findViewById(R.id.reset)
         val text: TextView = this.findViewById(R.id.result)
 
+
+        // start loop log
+        thread { count_loop() }
+
         var count = 0
         count_button.setOnClickListener { view: View? ->
-            count += 1
-            text.text = count.toString()
+            thread {
+                count += 1
+                text.text = count.toString()
+            }
         }
         reset_button.setOnClickListener { view: View? ->
             text.text = this.getString(R.string.initial_text)
             count = 0
         }
+
+    }
+
+    private fun count_loop(){
+        do {
+            for (i in 1..100) {
+                Log.d("MainActivity", i.toString())
+                sleep(1000)
+            }
+        } while(true)
     }
 }
